95723ab29f61ba0c5157189ffa31e5189d130491
aejmmark <aejmelaeus.markus@gmail.com>
2022-05-22 16:59:33 +0300

    Add basic package parsing and listing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import './App.css';
 import FileForm from './fileForm';
+import PackageList from './packageList';
 
 function App() {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState([]);
+
+  const parsePackages = (packages) => {
+    const parsedPackages = packages
+      .split('[[package]]')
+      .filter((pack) => pack.includes('description'))
+      .map((pack) => {
+        const splitLines = pack.split('\n');
+        const packData = {
+          name: splitLines[1].replace('name = ', ''),
+          description: splitLines[3].replace('description = ', ''),
+          optional: splitLines[5].includes('true'),
+        };
+        return packData;
+      });
+    return parsedPackages;
+  };
 
   const readFile = (newFile) => {
     const reader = new FileReader();
     reader.onloadend = () => {
       const contents = reader.result;
-      setFile(contents);
+      setFile(parsePackages(contents));
     };
     reader.readAsText(newFile);
   };
@@ -21,7 +38,7 @@ function App() {
   return (
     <div>
       <FileForm fileHandler={handleFileChange} />
-      {file !== null ? <p>{file}</p> : null}
+      <PackageList packages={file} />
     </div>
   );
 }
---
+++ src/packageList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function PackageList({ packages }) {
+  return (
+    <div>
+      <ul>
+        {packages
+          .sort((a, b) => a - b)
+          .map((pack) => (
+            <li key={pack.name}>{pack.name}</li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+PackageList.propTypes = {
+  packages: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
+
+export default PackageList;
