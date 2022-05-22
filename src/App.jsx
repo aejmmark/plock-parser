7a14cebc12bc622555a6086f8dd232d2b54df3de
aejmmark <aejmelaeus.markus@gmail.com>
2022-05-22 13:38:27 +0300

    Add eslint and fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <p>Hello world!</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
