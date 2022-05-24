7e34af0a0689beeeb23f18a187f70c42f916e35e
aejmmark <aejmelaeus.markus@gmail.com>
2022-05-24 11:51:12 +0300

    Add package information to package page

--- src/packagePage.jsx
+++ src/packagePage.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 function PackagePage({ packages }) {
   const selection = useParams().id;
   const selectedPackage = packages.find((pack) => pack.name === selection);
+  const reverseDependecies = packages
+    .filter((pack) => pack.dependencies.map((dep) => dep.name).includes(selection));
   return (
     <div>
-      <p>{selectedPackage.name}</p>
+      <p>
+        name:
+        {selectedPackage.name}
+      </p>
+      <p>
+        description:
+        {selectedPackage.description}
+      </p>
+      <p>dependencies:</p>
+      <ul>
+        {selectedPackage.dependencies.map((dep) => (
+          <li>
+            {packages.find((curr) => curr.name === dep.name) ? (
+              <Link to={`/${dep.name}`}>{dep.name}</Link>
+            ) : (
+              dep.name
+            )}
+            {dep.optional ? ' (optional)' : null}
+          </li>
+        ))}
+      </ul>
+      <p>reverse dependencies:</p>
+      <ul>
+        {reverseDependecies.map((dep) => (
+          <li>
+            <Link to={`/${dep.name}`}>{dep.name}</Link>
+            {dep.optional === 'true' ? ' (optional)' : null}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
